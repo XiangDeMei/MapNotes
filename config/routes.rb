0b6a810043673ff9340c8a7e5988b1bc5cc632d4
lpmvb <xiangdemei@yahoo.com>
2017-06-22 14:16:40 +0800

    finish home_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/help'
+  get '/help', to: 'pages#help'
+
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
